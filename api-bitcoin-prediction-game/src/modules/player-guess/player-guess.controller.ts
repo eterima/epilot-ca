a684cc036cc8752dea8e599aec89be4fac8f0a83
Amel Muminovic <amcenp@gmail.com>
2024-03-10 12:29:12 +0100

    feat: player guess controller connected

--- api-bitcoin-prediction-game/src/modules/player-guess/player-guess.controller.ts
+++ api-bitcoin-prediction-game/src/modules/player-guess/player-guess.controller.ts
@@ -1,12 +1,22 @@
-import { Body, Controller, Post, UseGuards, Request } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UseGuards,
+  Request,
+  Logger,
+} from '@nestjs/common';
 import { AuthGuard } from '../auth/auth.guard';
 import { RequestWithUser } from 'src/global.types';
 import { CreatePlayerGuessValidation } from './validation/create-player-guess.validation';
 import { createPlayerGuessValidationToDto } from './mappers/createPlayerGuessValidationToDto';
+import { PlayerGuessService } from './player-guess.service';
 
 @UseGuards(AuthGuard)
 @Controller('player-guesses')
 export class PlayerGuessController {
+  private readonly logger = new Logger(PlayerGuessController.name);
+  constructor(private playerGuessService: PlayerGuessService) {}
   @Post()
   async createPlayerGuess(
     @Request() req: RequestWithUser,
@@ -17,6 +27,18 @@ export class PlayerGuessController {
       createPlayerGuessInput,
       playerId,
     );
-    return createPlayerGuessDto;
+    this.logger.log(
+      'Request for creating player guess received',
+      createPlayerGuessDto,
+    );
+    try {
+      const playerGuess = await this.playerGuessService.createPlayerGuess(
+        createPlayerGuessDto,
+      );
+      return playerGuess;
+    } catch (error) {
+      // TODO - Unify errors
+      throw error;
+    }
   }
 }
--- api-bitcoin-prediction-game/src/modules/player-guess/player-guess.service.ts
+++ api-bitcoin-prediction-game/src/modules/player-guess/player-guess.service.ts
@@ -1,6 +1,7 @@
 import { Inject, Injectable, Logger } from '@nestjs/common';
 import { PlayerGuessRepository } from './player-guess.repository';
 import { PlayerGuessMongoDBRepositoryImplementation } from './database/player-guess.mongodb.repository';
+import { CreatePlayerGuessDTO } from './dtos/create-player-guess.dto';
 
 @Injectable()
 export class PlayerGuessService {
@@ -9,4 +10,18 @@ export class PlayerGuessService {
     @Inject(PlayerGuessMongoDBRepositoryImplementation)
     private playerGuessRepo: PlayerGuessRepository,
   ) {}
+
+  async createPlayerGuess(createPlayerGuessDto: CreatePlayerGuessDTO) {
+    try {
+      this.logger.log('Creating player guess', { createPlayerGuessDto });
+
+      // TODO - Validate that scheduled time is not off/hacky
+      const playerGuess = await this.playerGuessRepo.createPlayerGuess(
+        createPlayerGuessDto,
+      );
+      return playerGuess;
+    } catch (error) {
+      throw error;
+    }
+  }
 }
