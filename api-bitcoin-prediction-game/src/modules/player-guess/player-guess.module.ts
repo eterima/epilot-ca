117a65b0304e9e4ea347b18010d920a76882466b
Amel Muminovic <amcenp@gmail.com>
2024-03-10 12:15:15 +0100

    feat: db setup

--- api-bitcoin-prediction-game/src/modules/player-guess/player-guess.module.ts
+++ api-bitcoin-prediction-game/src/modules/player-guess/player-guess.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { PlayerGuessService } from './player-guess.service';
 import { PlayerGuessController } from './player-guess.controller';
+import { PlayerGuessMongoDBRepositoryImplementation } from './database/player-guess.mongodb.repository';
 
 @Module({
-  providers: [PlayerGuessService],
+  providers: [
+    PlayerGuessService,
+    {
+      provide: PlayerGuessMongoDBRepositoryImplementation,
+      useClass: PlayerGuessMongoDBRepositoryImplementation,
+    },
+  ],
   controllers: [PlayerGuessController],
 })
 export class PlayerGuessModule {}
--- api-bitcoin-prediction-game/src/modules/player-guess/player-guess.service.ts
+++ api-bitcoin-prediction-game/src/modules/player-guess/player-guess.service.ts
@@ -1,4 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
+import { PlayerGuessRepository } from './player-guess.repository';
+import { PlayerGuessMongoDBRepositoryImplementation } from './database/player-guess.mongodb.repository';
 
 @Injectable()
-export class PlayerGuessService {}
+export class PlayerGuessService {
+  private readonly logger = new Logger(PlayerGuessService.name);
+  constructor(
+    @Inject(PlayerGuessMongoDBRepositoryImplementation)
+    private playerGuessRepo: PlayerGuessRepository,
+  ) {}
+}
