1f3452384dc0dee6c2e4a19612d48f18163f3457
Amel Muminovic <amcenp@gmail.com>
2024-03-18 00:37:58 +0100

    feat: game start

--- client-bitcoin-prediction-game/src/pages/game/Game.tsx
+++ client-bitcoin-prediction-game/src/pages/game/Game.tsx
@@ -1,5 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+
+import "./Game.css";
 
 export const Game = () => {
-  return <div>Game</div>;
+  return (
+    <Container className="container game-container">
+      <Row>
+        <Col sm={12} md={6}>
+          <Row>
+            <Col>
+              <h5 className="text-success">Guess the price</h5>
+            </Col>
+          </Row>
+        </Col>
+        <Col sm={12} md={6}>
+          <Row>
+            <Col>
+              <h5 className="text-success">Previous guesses</h5>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
--- client-bitcoin-prediction-game/src/router/ProtectedRoutes/ProtectedRoutes.tsx
+++ client-bitcoin-prediction-game/src/router/ProtectedRoutes/ProtectedRoutes.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import { Navigate, Outlet } from "react-router-dom";
 
+// TODO - Add automatic redirection from login/register if user logged in already
 const ProtectedRoutes = () => {
   const localStorageToken = localStorage.getItem("accessToken");
 
